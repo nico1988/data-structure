a69903791c6b679a6d2d0cceb28490b756cc31ad
nico1988 <410827024@qq.com>
2019-09-26 17:25:21 +0800

    union find 孩子指向父亲

--- 11-Union-Find/04-Optimized-by-Size/src/Main.java
+++ 11-Union-Find/04-Optimized-by-Size/src/Main.java
@@ -10,13 +10,13 @@ public class Main {
         long startTime = System.nanoTime();
 
 
-        for(int i = 0 ; i < m ; i ++){
+        for(int i = 0 ; i < m ; i ++){ // 合并
             int a = random.nextInt(size);
             int b = random.nextInt(size);
-            uf.unionElements(a, b);
+            uf.unionElements(a, b); // 随机合并
         }
 
-        for(int i = 0 ; i < m ; i ++){
+        for(int i = 0 ; i < m ; i ++){ // 查询
             int a = random.nextInt(size);
             int b = random.nextInt(size);
             uf.isConnected(a, b);
@@ -37,10 +37,10 @@ public class Main {
         int size = 100000;
         int m = 100000;
 
-        UnionFind1 uf1 = new UnionFind1(size);
+        UnionFind1 uf1 = new UnionFind1(size); // java底层堆循环有优化 所以有可能比 UnionFind2 时间小
         System.out.println("UnionFind1 : " + testUF(uf1, m) + " s");
 
-        UnionFind2 uf2 = new UnionFind2(size);
+        UnionFind2 uf2 = new UnionFind2(size); // 索引操作 不是访问连续的内存空间
         System.out.println("UnionFind2 : " + testUF(uf2, m) + " s");
 
         UnionFind3 uf3 = new UnionFind3(size);
--- 11-Union-Find/04-Optimized-by-Size/src/UnionFind3.java
+++ 11-Union-Find/04-Optimized-by-Size/src/UnionFind3.java
@@ -2,7 +2,7 @@
 public class UnionFind3 implements UF{
 
     private int[] parent; // parent[i]表示第一个元素所指向的父节点
-    private int[] sz;     // sz[i]表示以i为根的集合中元素个数
+    private int[] sz;     // sz[i]表示以i为根的集合中元素个数  让小的树指向深的树
 
     // 构造函数
     public UnionFind3(int size){
@@ -52,9 +52,10 @@ public class UnionFind3 implements UF{
 
         if(pRoot == qRoot)
             return;
-
+        // 性能优化  减小树的深度变化幅度
         // 根据两个元素所在树的元素个数不同判断合并方向
         // 将元素个数少的集合合并到元素个数多的集合上
+
         if(sz[pRoot] < sz[qRoot]){
             parent[pRoot] = qRoot;
             sz[qRoot] += sz[pRoot];
